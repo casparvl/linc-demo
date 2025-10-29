4046f163997e957967e50391ad02af21d3dfdb87
Caspar van Leeuwen <casparl@tcn469.local.snellius.surf.nl>
2025-10-29 15:47:29 +0100

    Revert order to make sure wget is available

--- run_all.sh
+++ run_all.sh
@@ -5,11 +5,11 @@ if [ ! -d "/cvmfs/software.eessi.io" ]; then
     ./install_eessi.sh
 fi
 
-# Download data for HBA_target pipeline
-./download.sh
-
 # Load environment
 source load_linc.sh
+
+# Download data for HBA_target pipeline
+./download.sh
 
 # Patch the test-jobs/HBA_target.json to replace hardcoded /usr/lib
 ./patch_test_jobs.sh
